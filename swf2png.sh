9b951276ba73f03ff745e8e608c42607432ba3e6
gorm <proev@users.noreply.github.com>
2014-05-19 08:45:08 +0200

    use correct name

--- swf2png.sh
+++ swf2png.sh
@@ -3,23 +3,24 @@ function convertswf {
   echo '* * *' swfrender $1
   swfrender $1
   echo '* * *' cp output.png $1
-  cp output.png $1
+  pngfile=${1%swf}png
+  cp output.png $pngfile
 }
 
 function convertmeta {
   echo '* *' sed -i.bak s/.swf/.png/g meta.json
-  echo sed -i.bak s/.swf/.png/g meta.json
+  sed -i.bak s/.swf/.png/g meta.json
 }
 
 while read f;
 do
-  echo '*' cd video/$f/timeline
-  cd video/$f/timeline
+  echo '*' cd $f/timeline
+  cd $f/timeline
   for s in *.swf;
   do
     convertswf $s
   done
   cd ..
   convertmeta $f
-  cd ../..
-done < files
+  cd ..
+done
